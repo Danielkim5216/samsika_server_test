cdd48cb408fe7cfa8f52258df3c18a0943883fe2
Danielkim <danielkim5216@gmail.com>
2024-10-04 02:24:04 +0900

    file upload

---
+++ src/components-home/HomeFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeFooter = () => {
+  return (
+    <div className="Homefooter">
+      <p className="Homefooter__font">Copyright &copy; 삼식아.</p>
+    </div>
+  );
+};
+
+export default HomeFooter;
---
+++ src/components-main/MainFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainFooter = () => {
+  return (
+    <footer id="Mainfooter" className="Mainfooter">
+      <div className="Mainfooter__content">Copyright &copy; 삼식아.</div>
+    </footer>
+  );
+};
+
+export default MainFooter;
